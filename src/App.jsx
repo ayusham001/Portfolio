c35c5dd200b715d4f8cd19ee5a90912cfccc2b2f
Ayush Mishra <ayusham001@gmail.com>
2023-08-02 22:51:33 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import './index.css';
-import Skills from "./components/skills";
 
 function App() {
   const [isLightMode, setLightMode] = useState(false);
