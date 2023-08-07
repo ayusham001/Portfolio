cf4deee6b06fb352c240c67f2041707c5ccdc230
Ayush Mishra <ayusham001@gmail.com>
2023-08-07 12:10:11 +0530

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,7 +50,8 @@ export default function Contact({ isLightMode }) {
                     name="name"
                     value={formData.name}
                     onChange={handleChange}
-                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className={isLightMode ? "w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        : "w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"}
                     required
                 />
             </div>
